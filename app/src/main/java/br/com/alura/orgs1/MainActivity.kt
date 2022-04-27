820e53e41af8963752d19303bce1af6f3afda9f7
marques <marques.psm@gmail.com>
2022-04-27 20:53:06 -0300

    Criando as view

--- app/src/main/java/br/com/alura/orgs1/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs1/MainActivity.kt
@@ -1,6 +1,14 @@
 package br.com.alura.orgs1
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activety_main)
+    }
 }
