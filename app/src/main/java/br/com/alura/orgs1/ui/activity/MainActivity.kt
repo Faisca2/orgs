5e20c395f8f5895fc0bbeca8b268f7f90412435e
marques <marques.psm@gmail.com>
2022-04-29 11:51:05 -0300

    emprementando action booton

--- app/src/main/java/br/com/alura/orgs1/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs1/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs1.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,36 +9,39 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs1.R
 import br.com.alura.orgs1.ui.model.Produto
 import br.com.alura.orgs1.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activety_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.setText("Cesta de frutas")
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.setText("Laranja, Maçã, Uva")
-//        val valor     = findViewById<TextView>(R.id.valor)
-//        valor.setText("19.90")
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerView.adapter = ListaProdutosAdapter(
+        ListaProdutosAdapter(
             context = this, produtos = listOf(
                 Produto(
                     nome = "teste",
                     descricao = "teste desc",
                     valor = "19.99"
+//                    valor = BigDecimal("19.99")
                 ),
                 Produto(
                     nome = "teste_01",
                     descricao = "teste_01 desc",
                     valor = "29.99"
+//                    valor = BigDecimal("29.99")
                 ),
                 Produto(
                     nome = "teste_02",
                     descricao = "teste_02 desc",
                     valor = "39.99"
+//                    valor = BigDecimal("39.99")
                 ),
             )
-        )
-        //recyclerView.layoutManager = LinearLayoutManager(this)
+        ).also { recyclerView.adapter = it }
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/br/com/alura/orgs1/ui/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs1/ui/model/Produto.kt
@@ -1,7 +1,10 @@
 package br.com.alura.orgs1.ui.model
 
+import java.math.BigDecimal
+
 data class Produto(
     val nome: String,
     val descricao: String,
     val valor: String
+    //val valor:BigDecimal
 )
